f75b6e495d786e7a2335639d76639015b44656cf
Nelush Gayashan <nelushgayashan@gmail.com>
2025-04-15 22:04:13 +0530

    Initiating original state

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  base: '/smart-bill-calculator/',
-  plugins: [react()],
-})
+  plugins: [react(), tailwindcss()],
+});
